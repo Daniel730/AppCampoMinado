c26fbc3e7cc1952c11fee8f92ffa50693441bc64
Daniel Silva <daniel.silva@direitodeouvir.com.br>
2021-03-29 10:51:49 -0300

    Campos e minas

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import Field from './src/components/Field'
 import params from './src/params'
 
 export default class App extends Component{
   render(){
     return(
-      <View>
+      <View style={styles.container}>
         <Text>Iniciando o Mines!</Text>
         <Text>Tamanho da Grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={4} />
+        <Field opened nearMines={5} />
+        <Field opened nearMines={6} />
+        <Field opened nearMines={7} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
