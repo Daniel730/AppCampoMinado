dd20bea4527ce1e81c1ee1dbd81e37bf0518dff9
Daniel Silva <daniel.silva@direitodeouvir.com.br>
2021-03-29 10:00:56 -0300

    Projeto inicial

---
+++ src/params.js
@@ -0,0 +1,18 @@
+const { Dimensions } = require("react-native")
+
+const params ={
+    blockSize: 30,
+    borderSize: 5,
+    fontSize: 15,
+    headerRatio: 0.15, //proporção do painel superior na tela
+    difficultLevel: 0.1,
+    getColumnsAmount() {
+        const width = Dimensions.get('width').width
+        return Math.floor(width / this.blockSize)
+    },
+    getRowsAmount() {
+        const totalHeight = Dimensions.get('width').height
+        const boardHeight = totalHeight * (1 - this.headerRatio);
+        return Math.floor(boardHeight / this.blockSize)
+    }
+}
